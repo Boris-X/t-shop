062718dd7430239dd8bf6d993c477c92e54ec21f
PS2021SBCQGJBO\Admin <332718604@qq.com>
2022-01-15 12:45:09 +0800

    网页基础

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,53 @@
 <template>
   <div class="home">
     <h1>This is a home page</h1>
+    <h4>This is a home page</h4>
+    <div class="test">测试用</div>
+    <span>随便试试</span> <br />
+    <span>另外一行</span>
+    <div class="pic">
+      <a href="http://www.baidu.com"
+        ><img src="../assets/img/1.jpg" alt="" title="我是hunter"
+      /></a>
+    </div>
+    <a href="http://www.baidu.com" style="text-decoration: none">网络链接</a>
+
+    <table align="center" width="500" height="249" border="1" cellspacing="0" cellpadding="4">
+      <thead>
+        <tr>
+          <th>排名</th>
+          <th>关键词</th>
+          <th>趋势</th>
+          <th>进入搜索</th>
+          <th>最近七日</th>
+          <th>相关链接</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>三国演义</td>
+          <td><img src="" /></td>
+          <td>456</td>
+          <td>123</td>
+          <td>
+            <a href="#">贴吧</a> <a href="#">图片</a> <a href="#">百科</a>
+          </td>
+        </tr>
+
+        <tr>
+          <td>1</td>
+          <td>三国演义</td>
+          <td><img src="" /></td>
+          <td>456</td>
+          <td>123</td>
+          <td>
+            <a href="#">贴吧</a> <a href="#">图片</a> <a href="#">百科</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -41,15 +88,15 @@ computed(() => {
   }
 });
 */
-
 </script>
 
 <style>
-.container {
-  width: 100%;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
+.test {
+  font-size: 16px;
+  color: red;
+  text-align: center;
+}
+.pic {
+  text-align: center;
 }
 </style>
